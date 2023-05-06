5dd41b1f93f11e780b13bb21d90550da46812036
Grace Spletzer <gspletzer@influxdata.com>
2023-05-05 21:47:49 -0700

    chore: add additional dependencies

--- mix.exs
+++ mix.exs
@@ -49,7 +49,9 @@ defmodule BounceGs.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:mix_test_interactive, "~> 1.0", only: :dev, runtime: false},
+      {:money, "~> 1.12"}
     ]
   end
 
