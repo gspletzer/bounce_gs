7e252ea8097451b2650f93e9be3c4665c3964af1
Grace Spletzer <gspletzer@influxdata.com>
2023-05-07 21:05:55 -0700

    chore: disabled unused error tests

--- test/bounce_gs_web/controllers/error_json_test.exs
+++ test/bounce_gs_web/controllers/error_json_test.exs
@@ -1,12 +1,12 @@
-defmodule BounceGsWeb.ErrorJSONTest do
-  use BounceGsWeb.ConnCase, async: true
+# defmodule BounceGsWeb.ErrorJSONTest do
+#   use BounceGsWeb.ConnCase, async: true
 
-  test "renders 404" do
-    assert BounceGsWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
-  end
+#   test "renders 404" do
+#     assert BounceGsWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+#   end
 
-  test "renders 500" do
-    assert BounceGsWeb.ErrorJSON.render("500.json", %{}) ==
-             %{errors: %{detail: "Internal Server Error"}}
-  end
-end
+#   test "renders 500" do
+#     assert BounceGsWeb.ErrorJSON.render("500.json", %{}) ==
+#              %{errors: %{detail: "Internal Server Error"}}
+#   end
+# end
